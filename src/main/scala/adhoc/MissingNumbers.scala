5076ba7adcfce1e2d1723ae3af2e2145b58c5802
Karakaya <212457624@logon.ds.ge.com>
2016-07-17 01:28:52 +0300

    add missing numbers in scala

---
+++ src/main/scala/adhoc/MissingNumbers.scala
@@ -0,0 +1,30 @@
+package adhoc
+
+import scala.collection.mutable.ListBuffer
+import scala.io.StdIn
+
+/**
+  * Created by 212457624 on 15.07.2016.
+  */
+object MissingNumbers {
+
+  def main(args: Array[String]) {
+    var firstList=readList(StdIn.readInt())
+    var secondList=readList(StdIn.readInt())
+    val arr= new Array[Int](200000)
+    for ( i <- firstList) {
+      arr(i)=  arr(i) - 1
+    }
+    for ( i <- secondList) {
+      arr(i)=  arr(i) + 1
+    }
+    for ( i <- 0 to (arr.length - 1)) {
+      if(arr(i)>0){
+        print(i+" ")
+      }
+    }
+  }
+  def readList(size: Int)={
+    StdIn.readLine().split(" ").map(_.toInt)
+  }
+}
