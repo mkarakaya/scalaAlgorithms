3760915f141cc2f2966d2c616245927e8b90c08d
Karakaya <212457624@logon.ds.ge.com>
2016-07-31 22:57:12 +0300

    add StockMaximize

---
+++ src/main/scala/dynamicProgramming/StockMaximize.scala
@@ -0,0 +1,26 @@
+package dynamicProgramming
+
+import scala.io.StdIn
+
+/**
+  * https://www.hackerrank.com/challenges/stockmax
+  * Created by 212457624 on 31.07.2016.
+  */
+object StockMaximize {
+  def main(args: Array[String]): Unit = {
+    val tests = StdIn.readInt();
+    1 to tests foreach { _ =>
+      val size = StdIn.readInt();
+      val arr = StdIn.readLine().split(" ").map(_.toInt)
+      var profit = BigInt (0)
+      var max =  0
+      for (i <- arr.length - 1 to 0 by -1) {
+        if (arr(i) >= max) {
+          max = arr(i)
+        }
+        profit += max - arr(i)
+      }
+      println(profit)
+    }
+  }
+}
