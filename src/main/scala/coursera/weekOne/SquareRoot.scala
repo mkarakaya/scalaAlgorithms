2189681ee76f412831bb3ce6dc34a95364826a98
mokarakaya <mozgkarakaya@gmail.com>
2017-06-20 23:26:10 +0200

    add SquareRoot

--- src/main/scala/coursera/weekOne/SquareRoot.scala
+++ src/main/scala/coursera/weekOne/SquareRoot.scala
@@ -6,7 +6,23 @@ package coursera.weekOne
 object SquareRoot {
 
   def main(args: Array[String]) {
-
+    print(sqrt(1.0e50))
   }
 
+  def abs(x: Double) = if(x<0) -x else x
+
+  def sqrt(x: Double) = {
+
+    def isGoodEnough(guess: Double): Boolean =
+      abs(guess * guess - x) /x  < 0.001
+
+    def improve(guess: Double): Double =
+      (guess + x / guess) /2
+
+    def sqrtIter(guess: Double): Double =
+      if(isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
+
+    sqrtIter(1)
+  }
 }
